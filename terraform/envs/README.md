e8e635d203be568ce418540caa78a6c66d4e7ef7
nao-kichi <98793084+nao-kichi@users.noreply.github.com>
2026-04-03 15:07:03 +0900

    docs(terraform): envs README の構成見直しと cloudfront.tf の可読性向上

--- terraform/envs/README.md
+++ terraform/envs/README.md
@@ -1,22 +1,20 @@
-# terraform関連でgitにアップロードするものとそうでないもの。
-・.tf系のファイル → アップロードする（OK）
-・tfstate → アップロードしない（絶対NG）
+# terraformの手順書
+
+## terraform関連でgitにアップロードするものとそうでないもの。
+・.tf系のファイル → Gitアップロードする（OK）
+・tfstate → Gitアップロードしない（絶対NG）
 ・.lock.hcl → 基本はアップロードする（←ここだけ違う）
 
-# 事前準備（不要なものを削除）
+## 事前準備（不要なものを削除）
 cd terraform/envs/prd
 rm -rf .terraform
 rm -f .terraform.lock.hcl
 
-# 初回 or 壊れた時だけ
+## 初回 or 壊れた時だけ
 terraform init
 
-# 定期実行の準備
+## 定期実行の準備
 terraform plan
 
-# 既存リソースの設定を自動で書き出すコマンド
-# generated.tf が存在する場合、中身が新しく生成されて上書きされる
-terraform plan -generate-config-out=generated.tf
-
-# tfの状態にAWSを合わせる（問題なければ実行）
+## tfの状態にAWSを合わせる（問題なければ実行）
 terraform apply
--- terraform/envs/prd/cloudfront.tf
+++ terraform/envs/prd/cloudfront.tf
@@ -8,16 +8,19 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
   price_class     = "PriceClass_All"
   web_acl_id      = "arn:aws:wafv2:us-east-1:077793792738:global/webacl/CreatedByCloudFront-a381b2b7/1a0436dc-a81e-49c8-91fb-2ce3852507cd"
 
+  # Tags
   tags = {
     Name = "more-coding-cdn-prd"
   }
 
+  # origins
   origin {
     domain_name              = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com"
     origin_id                = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
     origin_access_control_id = "E1RZ1IG3NWYRD3"
   }
 
+  # Behaviors
   default_cache_behavior {
     target_origin_id       = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
     viewer_protocol_policy = "redirect-to-https"
@@ -27,6 +30,7 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
+  # Behaviors
   ordered_cache_behavior {
     path_pattern             = "/questions/*"
     target_origin_id         = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
@@ -38,6 +42,7 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
     response_headers_policy_id = "eaab4381-ed33-4a86-88ca-d9558dc6cd63"
   }
 
+  # CloudFront全体へのアクセスを国単位で制御している
   restrictions {
     geo_restriction {
       restriction_type = "none"
@@ -45,12 +50,14 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
     }
   }
 
+  # HTTPS通信の設定（証明書とセキュリティレベル）
   viewer_certificate {
     cloudfront_default_certificate = true
     minimum_protocol_version       = "TLSv1"
   }
 }
 
+# すでにAWSにあるリソースをTerraformに取り込む
 import {
   to = aws_cloudfront_distribution.cdn_prd
   id = "E7FYRVVFZPAQ7"
