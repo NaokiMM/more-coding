e3bee92b09ffe0ad7bc2eee55a8ee44e7f31aab8
nao-kichi <98793084+nao-kichi@users.noreply.github.com>
2026-04-07 22:11:35 +0900

    docs(terraform): prd apigateway の CORS/import/authorizer 説明を追記

--- terraform/envs/prd/cloudfront.tf
+++ terraform/envs/prd/cloudfront.tf
@@ -2,25 +2,43 @@
 # CloudFront Distributions
 # --------------------
 resource "aws_cloudfront_distribution" "cdn_prd" {
+  # --------------------
+  # General
+  # --------------------
   enabled         = true
   is_ipv6_enabled = true
   comment         = "more-coding-cdnのprd"
   price_class     = "PriceClass_All"
-  web_acl_id      = "arn:aws:wafv2:us-east-1:077793792738:global/webacl/CreatedByCloudFront-a381b2b7/1a0436dc-a81e-49c8-91fb-2ce3852507cd"
 
-  # Tags
-  tags = {
-    Name = "more-coding-cdn-prd"
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1"
   }
 
-  # origins
+  # --------------------
+  # Security
+  # --------------------
+  web_acl_id = "arn:aws:wafv2:us-east-1:077793792738:global/webacl/CreatedByCloudFront-a381b2b7/1a0436dc-a81e-49c8-91fb-2ce3852507cd"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  # --------------------
+  # Origins
+  # --------------------
   origin {
     domain_name              = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com"
     origin_id                = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
     origin_access_control_id = "E1RZ1IG3NWYRD3"
   }
 
+  # --------------------
   # Behaviors
+  # --------------------
   default_cache_behavior {
     target_origin_id       = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
     viewer_protocol_policy = "redirect-to-https"
@@ -30,7 +48,6 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
-  # Behaviors
   ordered_cache_behavior {
     path_pattern               = "/questions/*"
     target_origin_id           = "more-coding-learning-content-prd.s3.ap-northeast-1.amazonaws.com-mltm94cf23z"
@@ -42,18 +59,20 @@ resource "aws_cloudfront_distribution" "cdn_prd" {
     response_headers_policy_id = "eaab4381-ed33-4a86-88ca-d9558dc6cd63"
   }
 
-  # CloudFront全体へのアクセスを国単位で制御している
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-      locations        = []
-    }
+  # --------------------
+  # Logging
+  # --------------------
+  logging_config {
+    include_cookies = false
+    bucket          = "more-coding-cloudfront-logs-prd.s3.amazonaws.com"
+    prefix          = "cloudfront/"
   }
 
-  # HTTPS通信の設定（証明書とセキュリティレベル）
-  viewer_certificate {
-    cloudfront_default_certificate = true
-    minimum_protocol_version       = "TLSv1"
+  # --------------------
+  # Tags
+  # --------------------
+  tags = {
+    Name = "more-coding-cdn-prd"
   }
 }
 
--- terraform/envs/prd/cognito.tf
+++ terraform/envs/prd/cognito.tf
@@ -1,14 +1,50 @@
 # --------------------
-# Cognito User Pools
+# Cognito User Pool
 # --------------------
 resource "aws_cognito_user_pool" "more_coding_prd" {
+  # --------------------
+  # Overview / Basic
+  # --------------------
   name                = "more-coding-prd"
   deletion_protection = "ACTIVE"
   mfa_configuration   = "OFF"
 
-  auto_verified_attributes = ["email"]
-  username_attributes      = ["email"]
+  # --------------------
+  # Applications
+  # --------------------
+  # App clients はこのresourceでは管理しない。
+  # Terraformでは aws_cognito_user_pool_client として別resourceで定義するため。
+  # (例: more-coding-prd-user)
 
+  # --------------------
+  # Authentication > Sign-in
+  # --------------------
+  username_attributes = ["email"]
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  sign_in_policy {
+    allowed_first_auth_factors = ["PASSWORD"]
+  }
+
+  # --------------------
+  # Authentication > Sign-up
+  # --------------------
+  auto_verified_attributes = ["email"]
+
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+  }
+
+  # --------------------
+  # Security
+  # --------------------
   account_recovery_setting {
     recovery_mechanism {
       name     = "verified_email"
@@ -18,14 +54,6 @@ resource "aws_cognito_user_pool" "more_coding_prd" {
       name     = "verified_phone_number"
       priority = 2
     }
-  }
-
-  admin_create_user_config {
-    allow_admin_create_user_only = false
-  }
-
-  email_configuration {
-    email_sending_account = "COGNITO_DEFAULT"
   }
 
   password_policy {
@@ -38,16 +66,11 @@ resource "aws_cognito_user_pool" "more_coding_prd" {
     temporary_password_validity_days = 7
   }
 
-  sign_in_policy {
-    allowed_first_auth_factors = ["PASSWORD"]
-  }
-
-  username_configuration {
-    case_sensitive = false
-  }
-
-  verification_message_template {
-    default_email_option = "CONFIRM_WITH_CODE"
+  # --------------------
+  # Branding
+  # --------------------
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
   }
 }
 
